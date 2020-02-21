5a6ab7a28896faa9af680718314c2e22258a841d
Strontium233 <metako213@gmail.com>
2020-02-21 04:01:55 -0500

    feat(UI) fixed the preloader

--- client/src/react-components/StagesModal/index.js
+++ client/src/react-components/StagesModal/index.js
@@ -5,7 +5,6 @@ import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import {setState} from 'statezero';
 import Tooltip from '@material-ui/core/Tooltip';
-import './style.css';
 
 
 class StagesModal extends BaseComponent {
