cfd30a83e27237514f3891de518a94659099fde0
yanandfish <qc_3ea645188b004ddca236ca0500170d0c@git.code.tencent.com>
2020-03-18 16:35:00 +0800

    make sprite smaller

--- client/src/index.js
+++ client/src/index.js
@@ -16,4 +16,7 @@ ReactDOM.render(<App/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//if(process.env.NODE_ENV !== 'production')
+    serviceWorker.unregister();
+//else
+//    serviceWorker.register();
