f29cebdc3ab94b138002af5de329149e5adba04f
yanandfish <qc_3ea645188b004ddca236ca0500170d0c@git.code.tencent.com>
2020-04-03 18:17:07 +0800

    update service worker

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,9 +1,9 @@
 /**
  * @description Ark One Graph server worker
- * @version 0.0.1
+ * @version 0.1.1
  */
 
-importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js");
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js');
 
 /**
  * Configure
@@ -11,55 +11,101 @@ importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw
 
 // Set workbox config
 workbox.setConfig({
-  modulePathPrefix: '"https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/',
-  debug: true
-})
+  modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/',
+  debug: false
+});
 
 // Set workbox cache names
 workbox.core.setCacheNameDetails({
   prefix: 'aog:',
   suffix: 'v1'
-})
+});
 
 // Start controlling any existing clients as soon as it activates
-workbox.core.clientsClaim()
+workbox.core.clientsClaim();
 
 // Skip over the SW waiting lifecycle stage
-workbox.core.skipWaiting()
+workbox.core.skipWaiting();
 
 // Remove unused
-workbox.precaching.cleanupOutdatedCaches()
+workbox.precaching.cleanupOutdatedCaches();
 
 /**
  * Precaches
  */
 
-const hostList=['aog.wiki','en.aog.wiki','arkonegraph.herokuapp.com'/* ,'localhost','localhost:3000' */]
+const hostList = ['en.aog.wiki', 'aog.wiki', 'arkonegraph.herokuapp.com'];
 
 // Cache html
-woekbox.routing.registerRoute(
-  function (event) {
-    if (hostList.indexOf(event.url.host)) {
-      return /\/$/.test(event.url.pathname)
+workbox.routing.registerRoute(
+  function(event) {
+    if (~hostList.indexOf(event.url.host)) {
+      return /(\/|.html)$/.test(event.url.pathname);
+    } else {
+      return false;
     }
-    else {
-      return false
-    }
-  }, new workbox.strategies.NetworkFirst({
-    cacheName: 'aog:html', plugins: [new workbox.cacheableResponse.Plugin({
-      statuses: [0, 200]
-    })]
+  },
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'aog:html',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
   })
-)
+);
 
 // Cache assets
-woekbox.routing.registerRoute(
+workbox.routing.registerRoute(
   new RegExp('/static'),
-  new woekbox.strategies.CacheFirst({
-    cacheName: 'aog:static', plugins: [new workbox.cacheableResponse.Plugin({
-      statuses: [0, 200]
-    })]
+  new workbox.strategies.CacheFirst({
+    cacheName: 'aog:static',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 24 * 60 * 60
+      })
+    ]
   })
-)
+);
+
+workbox.routing.registerRoute(
+  function(event) {
+    if (~hostList.indexOf(event.url.host)) {      
+      return RegExp('^/[^/]+\\.(?:png|ico)', 'i').test(event.url.pathname);
+    } else {
+      return false;
+    }
+  },
+  new workbox.strategies.CacheFirst({
+    cacheName: 'aog:static',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 24 * 60 * 60
+      })
+    ]
+  })
+);
+
+// Cache fonts
+workbox.routing.registerRoute(
+  new RegExp('.*\\.(?:woff2|woff|ttf)$', 'i'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'aog:font',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  })
+);
 
 // Cache data (BETA)
